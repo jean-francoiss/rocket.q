544ce002c43d4ead973d9db77428f5640394f53b
jean-francoiss <dimerhacker@gmail.com>
2022-05-17 06:56:33 -0300

    projeto finalizado

--- src/server.js
+++ src/server.js
@@ -1,9 +1,17 @@
-const extress = require('express')
+const express = require('express');
+const route = require('./route');
+const path = require('path');
 
-const server = express()
+const server = express();
 
-server.listen(3000, () => console.log("RODANDO"))
+server.set('view engine', 'ejs');
 
-function open(){
-    //abrirmodal
-}
+server.use(express.static("public"));
+
+server.set('views', path.join(__dirname, 'views'));
+
+server.use(express.urlencoded({extended: true}));
+
+server.use(route);
+
+server.listen(3000, () => console.log("RODANDO"));
